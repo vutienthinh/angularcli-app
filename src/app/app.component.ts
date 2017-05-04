609ef3ec30cd90dd40afa2ebef28b71bd0f2fec8
Thinh <you@example.com>
2017-05-04 22:47:17 +0700

    u

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CountryComponentComponent } from '../app/pages/country-component/country-component.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  entryComponents: [CountryComponentComponent],
 })
 export class AppComponent {
   title = 'app works!';
